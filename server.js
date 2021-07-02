124fcbd2baf0bee5fb202bf395b4924228279ea5
Mohammed Sayeed <sayeedofficial6220@gmail.com>
2021-07-02 09:04:59 +0530

    Added Posts Funtionality

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const app = epxress();
 app.use(epxress.json());
 const connectDB = require("./DB/db");
 
+//CRUD User Management System Routes
 const authRoute = require("./Routes/auth");
 const userRoute = require("./Routes/users");
+
+//CRUD Posts Management System Routes
+const postRoute = require("./Routes/posts");
 
 connectDB();
 
@@ -15,6 +19,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 const port = 5000;
 app.listen(port, () => console.log(`Server running at PORT ${port}`));
